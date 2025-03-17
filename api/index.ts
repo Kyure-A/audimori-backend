e38dbfa8f44299740a670a576ca7708ec69e104b
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-03-17 20:50:49 +0900

    feat: add request to openai

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import OpenAI from 'openai'
 import { Bindings } from './types'
+import { prompt } from './prompt'
 
 export const config = {
   runtime: 'edge'
@@ -14,10 +15,26 @@ app.get('/', (c) => {
 })
 
 app.post('/', async (c) => {
-    const { text } = await c.req.parseBody();
-    const ai = new OpenAI({
+    const text = c.req.query("text") as string;
+    
+    const openai = new OpenAI({
         apiKey: c.env.OPENAI_API_KEY
     })
+    
+    try {
+        const lyrics = (await openai.chat.completions.create({
+            model: "gpt-4o-mini",
+            messages: [
+                { role: "system", content: prompt },
+                { role: "user", content: text }
+            ]
+        })).choices[0].message;
+
+        return c.json({ success: true, value: lyrics, error: null }, 200)
+    } catch (e) {
+        console.log(e);
+        return c.json({ success: false, value: null, error: e }, 500)
+    }
 })
 
 export default handle(app)
--- api/types.ts
+++ api/types.ts
@@ -1,3 +1,7 @@
 export type Bindings = {
     OPENAI_API_KEY: string
 }
+
+export type Status =
+    | { success: true, value: string, error: null }
+    | { success: false, value: null, error: string }
